42c5bc786dd8c91210d30515176e2767072ef782
alehee <aleksanderheese@o2.pl>
2026-03-25 11:14:57 +0100

    init

---
+++ SimpleMetricsMod.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace CSL_SimpleMetrics
+{
+    public class SimpleMetricsMod : IUserMod
+    {
+        public string Name => "CSL-SimpleMetrics";
+        public string Description => "Modification for Cities:Skylines adding simple metrics bar to UI";
+    }
+}
