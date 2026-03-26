e2d10e7f3e2bf7e5fe691ac4118aabdcbc9ec2c2
alehee <aleksanderheese@o2.pl>
2026-03-26 19:58:25 +0100

    Logging and loading

---
+++ Extensions/LoadingExtension.cs
@@ -0,0 +1,23 @@
+﻿using CSL_SimpleMetrics.Logging;
+using ICities;
+
+namespace CSL_SimpleMetrics.Extensions
+{
+    public class LoadingExtension : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame)
+                return;
+
+            Logger.Log("Loaded moddification.");
+            // TODO: add initialization code here
+        }
+
+        public override void OnLevelUnloading()
+        {
+            Logger.Log("Unloaded moddification.");
+            // TODO: add cleanup code here
+        }
+    }
+}
---
+++ Logging/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace CSL_SimpleMetrics.Logging
+{
+    public class Logger
+    {
+        public static void Log(string message, LogLevelEnum logLevel = LogLevelEnum.Info)
+        {
+            Debug.Log($"[CSL-SimpleMetrics][{logLevel}] {message}");
+        }
+
+        public static void LogException(string message, Exception ex)
+        {
+            Debug.LogError($"[CSL-SimpleMetrics][{LogLevelEnum.Error}] {message}: {ex.Message}");
+        }
+    }
+}
