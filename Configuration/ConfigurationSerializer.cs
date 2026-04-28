534888d20752e91157d0be46182808fb2fb7334c
alehee <aleksanderheese@o2.pl>
2026-04-28 19:09:32 +0200

    configuration baby steps

---
+++ Configuration/ConfigurationSerializer.cs
@@ -0,0 +1,15 @@
+﻿namespace CSL_SimpleMetrics.Configuration
+{
+    public class ConfigurationSerializer
+    {
+        public void Save()
+        {
+            // TODO implement
+        }
+
+        public void Load()
+        {
+            // TODO implement
+        }
+    }
+}
--- Configuration/ConstConfiguration.cs
+++ Configuration/ConstConfiguration.cs
@@ -3,6 +3,7 @@
     public class ConstConfiguration
     {
         public static string AppPrefix => "CSL_SimpleMetrics";
+        public static string ConfigurationFileName => $"{AppPrefix}_Config.xml";
         public static float Height => 36;
         public static float Width => 558.5f;
         public static float IndicatorOpacity => 0.3f;
